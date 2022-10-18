b6b89e83cfc2e65fa20e9c165fbea26dafa35e79
Quyen Nguyen <quyenlearntocode@gmail.com>
2022-10-18 16:56:23 -0400

    updated design

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ function menuShow() {
 // settings
 const settingBtn = document.querySelector(".setting-btn");
 const themeBtn = document.querySelector(".theme-btn");
+const topBtn = document.querySelector(".top-btn");
+const downBtn = document.querySelector(".down-btn");
 
 settingBtn.addEventListener("click", settingShow);
 
@@ -29,35 +31,41 @@ let settingOpen = false;
 function settingShow() {
   if (!settingOpen) {
     themeBtn.classList.add("open");
+    topBtn.classList.add("open");
+    downBtn.classList.add("open");
     settingOpen = true;
   } else {
     themeBtn.classList.remove("open");
+    topBtn.classList.remove("open");
+    downBtn.classList.remove("open");
     settingOpen = false;
   }
 }
 
 // dark/light theme
 const themeCheckBox = document.querySelector(".theme-checkbox");
+const menuLinkTheme = document.querySelectorAll(".menu-link");
 const aboutTheme = document.querySelector("#about");
 const sectionNavTheme = document.querySelector(".section-nav");
 const featureTheme = document.querySelector("#features");
-const toolTheme = document.querySelector("#tools");
-const menuLinkTheme = document.querySelectorAll(".menu-link");
-const contactLinkTheme = document.querySelectorAll(".contact-link");
 const searchInputTheme = document.querySelector(".search-input");
 const searchBtnTheme = document.querySelector(".search-btn");
+const contactTheme = document.querySelector("#contacts");
+const contactLinkTheme = document.querySelectorAll(".contact-link");
 
 themeCheckBox.addEventListener("change", themeChange);
 
 function themeChange() {
   document.body.classList.toggle("light");
+  menuNav.classList.toggle("light");
+  menuLinkTheme.forEach(item => item.classList.toggle("light"));
   aboutTheme.classList.toggle("light");
   sectionNavTheme.classList.toggle("light");
   featureTheme.classList.toggle("light");
-  toolTheme.classList.toggle("light");
-  menuNav.classList.toggle("light");
-  menuLinkTheme.forEach(item => item.classList.toggle("light"));
-  contactLinkTheme.forEach(item => item.classList.toggle("light"));
   searchInputTheme.classList.toggle("light");
   searchBtnTheme.classList.toggle("light");
+  contactTheme.classList.toggle("light");
+  contactLinkTheme.forEach(item => item.classList.toggle("light"));
+  topBtn.classList.toggle("light");
+  downBtn.classList.toggle("light");
 }
