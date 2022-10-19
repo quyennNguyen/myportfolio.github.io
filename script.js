1e39a85f113cb86ec0eda86e691bc22c34878153
Quyen Nguyen <quyenlearntocode@gmail.com>
2022-10-19 00:47:02 -0400

    updated design

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 // menu
-const menuBtn = document.querySelector(".menu-btn")
-const menuNav = document.querySelector(".menu-nav")
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
 
-menuBtn.addEventListener("click", menuShow);
+menuBtn.addEventListener("click", () => {
+  menuNav.classList.toggle("open");
+});
 
-let menuOpen = false;
+// profile
+const profileBtn = document.querySelector(".profile-btn");
+const profile = document.querySelector(".profile");
+const objBtn = document.querySelector(".obj-btn");
+const obj = document.querySelector(".obj");
+const edBtn = document.querySelector(".ed-btn");
+const ed = document.querySelector(".ed");
+const techBtn = document.querySelector(".tech-btn");
+const tech = document.querySelector(".tech");
 
-function menuShow() {
-  if (!menuOpen) {
-    menuBtn.classList.add("click");
-    menuNav.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("click");
-    menuNav.classList.remove("open");
-    menuOpen = false;
-  }
-}
+profileBtn.addEventListener("click", () => {
+  profile.classList.toggle("open");
+  obj.classList.add("open");
+  ed.classList.remove("open");
+  tech.classList.remove("open");
+});
+objBtn.addEventListener("click", () => {
+  obj.classList.add("open");
+  ed.classList.remove("open");
+  tech.classList.remove("open");
+});
+edBtn.addEventListener("click", () => {
+  obj.classList.remove("open");
+  ed.classList.add("open");
+  tech.classList.remove("open");
+});
+techBtn.addEventListener("click", () => {
+  obj.classList.remove("open");
+  ed.classList.remove("open");
+  tech.classList.add("open");
+});
 
 // settings
 const settingBtn = document.querySelector(".setting-btn");
@@ -24,48 +44,51 @@ const themeBtn = document.querySelector(".theme-btn");
 const topBtn = document.querySelector(".top-btn");
 const downBtn = document.querySelector(".down-btn");
 
-settingBtn.addEventListener("click", settingShow);
-
-let settingOpen = false;
-
-function settingShow() {
-  if (!settingOpen) {
-    themeBtn.classList.add("open");
-    topBtn.classList.add("open");
-    downBtn.classList.add("open");
-    settingOpen = true;
-  } else {
-    themeBtn.classList.remove("open");
-    topBtn.classList.remove("open");
-    downBtn.classList.remove("open");
-    settingOpen = false;
-  }
-}
+settingBtn.addEventListener("click", () => {
+    themeBtn.classList.toggle("open");
+    topBtn.classList.toggle("open");
+    downBtn.classList.toggle("open");
+});
 
 // dark/light theme
 const themeCheckBox = document.querySelector(".theme-checkbox");
 const menuLinkTheme = document.querySelectorAll(".menu-link");
 const aboutTheme = document.querySelector("#about");
-const sectionNavTheme = document.querySelector(".section-nav");
+const profileTheme = document.querySelector(".section-nav");
 const featureTheme = document.querySelector("#features");
 const searchInputTheme = document.querySelector(".search-input");
 const searchBtnTheme = document.querySelector(".search-btn");
 const contactTheme = document.querySelector("#contacts");
 const contactLinkTheme = document.querySelectorAll(".contact-link");
 
-themeCheckBox.addEventListener("change", themeChange);
-
-function themeChange() {
+themeCheckBox.addEventListener("change", () => {
   document.body.classList.toggle("light");
   menuNav.classList.toggle("light");
-  menuLinkTheme.forEach(item => item.classList.toggle("light"));
+  menuLinkTheme.forEach((item) => item.classList.toggle("light"));
   aboutTheme.classList.toggle("light");
   sectionNavTheme.classList.toggle("light");
   featureTheme.classList.toggle("light");
   searchInputTheme.classList.toggle("light");
   searchBtnTheme.classList.toggle("light");
   contactTheme.classList.toggle("light");
-  contactLinkTheme.forEach(item => item.classList.toggle("light"));
+  contactLinkTheme.forEach((item) => item.classList.toggle("light"));
   topBtn.classList.toggle("light");
   downBtn.classList.toggle("light");
-}
+});
+
+// function clickable(element1, element2) {
+//   window.onclick = function(event) {
+//     if (!event.target.matches(element1)) {
+//       var targetEl = document.getElementsByClassName(element2);
+//       var i;
+  
+//       for (i = 0; i < targetEl.length; i++) {
+//         var openEl = targetEl[i];
+  
+//         if (openEl.classList.contains('open')) {
+//           openEl.classList.remove('open');
+//         }
+//       }
+//     }
+//   }
+// }
